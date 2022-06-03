4881cd297d574ee2c8b21848cdc87eae42d77bbc
Pawel Wujczyk <pwujczyk@hotmail.com>
2022-06-03 06:41:11 +0200

    readonly and editable

--- src/Components/BrickItem/index.js
+++ src/Components/BrickItem/index.js
@@ -1,7 +1,39 @@
-function BrickItem(props){
+import { useState } from "react";
+
+function BrickItem(props) {
     console.log("BrickItem");
     console.log(props)
-    
-    return (<p>{props.bricks.key} - {props.bricks.value}</p>)
+
+    const [mode, setMode] = useState('readonly')
+
+    const renderReadonly = () => {
+        return (
+            <div>
+                <p> {props.bricks.key} - {props.bricks.value}</p >
+                <button onClick={()=>setMode('editable')}>edit</button>
+            </div>
+        )
+    }
+
+    const renderEditable=()=>{
+        return(
+            <div>
+                <p>
+                    <input type="text"></input>
+                    <input type="text"></input>
+                </p>
+                <button onClick={()=>setMode('readonly')}>save</button>
+            </div>
+        )
+    }
+
+
+    if (mode == 'readonly') {
+        return renderReadonly();
+    }
+    else{
+        return renderEditable();
+    }
+
 }
 export default BrickItem
